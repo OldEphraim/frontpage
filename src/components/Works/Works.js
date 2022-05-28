7dbcfc926408385135d9b3c4eba66d85516fe430
oldephraim <a8garber@yahoo.com>
2022-05-28 13:52:17 -0400

    adding blogpost links

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -10,19 +10,6 @@ const Works = () => {
     return (
         <div className="works" id="Works">
                 <div className="cards">
-                    <img src={imageHeader} alt="" />
-                    <div className="project-explanation">
-                      <li>Landing page for Sunnyside Agency</li>
-                      <li>Built with HTML, CSS, and JavaScript</li>
-                      <li>Responsive web design</li>
-                    </div>
-                    <div className="button-container">
-                    <a href="https://oldephraim.github.io/Landing-Page/" target="_blank" rel="noreferrer"><button>See Project</button></a>
-                    <a href="https://github.com/OldEphraim/Landing-Page/" target="_blank" rel="noreferrer"><button>See GitHub</button></a>
-                    <button>See Blogpost</button>
-                    </div>
-                </div>
-                <div className="cards">
                     <img src={streetMap} alt="" />
                     <div className="project-explanation">
                       <li>Travel advisory website</li>
@@ -32,7 +19,7 @@ const Works = () => {
                     <div className="button-container">
                     <a href="https://oldephraim.github.io/Google-Travel-Advisory" target="_blank" rel="noreferrer"><button>See Project</button></a>
                     <a href="https://github.com/OldEphraim/Google-Travel-Advisory" target="_blank" rel="noreferrer"><button>See GitHub</button></a>
-                    <button>See Blogpost</button>
+                    <a href="https://oldephraimlearnstocode.wordpress.com/2022/05/25/travel-advisory-project-code-review/" target="_blank" rel="noreferrer"><button>See Blogpost</button></a>
                     </div>
                 </div>
                 <div className="cards">
@@ -44,7 +31,7 @@ const Works = () => {
                    <div className="button-container">
                    <a href="https://oldephraim.github.io/Word-Quiz" target="_blank" rel="noreferrer"><button>See Project</button></a>
                    <a href="https://github.com/OldEphraim/Word-Quiz" target="_blank" rel="noreferrer"><button>See GitHub</button></a>
-                   <button>See Blogpost</button>
+                   <a href="https://oldephraimlearnstocode.wordpress.com/2022/05/27/word-quiz-code-review/" target="_blank" rel="noreferrer"><button>See Blogpost</button></a>
                    </div>
                 </div>
                 <div className="cards">
@@ -57,8 +44,21 @@ const Works = () => {
                    <div className="button-container">
                    <a href="https://oldephraim.github.io/React-Wordle" target="_blank" rel="noreferrer"><button>See Project</button></a>
                    <a href="https://github.com/OldEphraim/React-Wordle" target="_blank" rel="noreferrer"><button>See GitHub</button></a>
-                   <button>See Blogpost</button>
+                   <a href="https://oldephraimlearnstocode.wordpress.com/2022/05/26/swordle-code-review/" target="_blank" rel="noreferrer"><button>See Blogpost</button></a>
                    </div>
+                </div>
+                <div className="cards">
+                    <img src={imageHeader} alt="" />
+                    <div className="project-explanation">
+                      <li>Landing page for Sunnyside Agency</li>
+                      <li>Built with HTML, CSS, and JavaScript</li>
+                      <li>Responsive web design</li>
+                    </div>
+                    <div className="button-container">
+                    <a href="https://oldephraim.github.io/Landing-Page/" target="_blank" rel="noreferrer"><button>See Project</button></a>
+                    <a href="https://github.com/OldEphraim/Landing-Page/" target="_blank" rel="noreferrer"><button>See GitHub</button></a>
+                    <a href="https://oldephraimlearnstocode.wordpress.com/2022/05/27/landing-page-code-review/" target="_blank" rel="noreferrer"><button>See Blogpost</button></a>
+                    </div>
                 </div>
             </div>
     )
